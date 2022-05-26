7a9830faa50e676420d995aa0de120ac0b74ae8b
David C. Streeter <dcstreeter@pbs.org>
2022-05-26 16:35:49 -0400

    four

--- src/Four.elm
+++ src/Four.elm
@@ -1,42 +1,96 @@
 module Four exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Browser.Events exposing (onAnimationFrame)
+import Html exposing (Html, button, div, input, meter, text)
+import Html.Attributes exposing (style, type_, value)
+import Html.Events exposing (onClick, onInput)
+import Time exposing (Posix)
 
 
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    -- This also works and could be used for smoother animation
+    -- onAnimationFrame Tick
+    Time.every 100 Tick
 
 
 type alias Model =
-    Int
+    { sliderValue : String
+    , elapsedTime : Int
+    }
 
 
-init : Model
-init =
-    0
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel, Cmd.none )
+
+
+initialModel : Model
+initialModel =
+    { sliderValue = "10"
+    , elapsedTime = 0
+    }
 
 
 type Msg
-    = Increment
-    | Decrement
+    = ChangeSlider String
+    | ResetTimer
+    | Tick Posix
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            model + 1
+        ChangeSlider s ->
+            ( { model | sliderValue = s }, Cmd.none )
 
-        Decrement ->
-            model - 1
+        ResetTimer ->
+            ( { model | elapsedTime = 0 }, Cmd.none )
+
+        Tick p ->
+            ( { model | elapsedTime = model.elapsedTime + 1 }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
+        [ div [ style "display" "inline" ]
+            [ text "Elapsed Time: "
+            , meter
+                [ style "width" "160px"
+                , Html.Attributes.min "0"
+                , value (String.fromInt model.elapsedTime)
+                , Html.Attributes.max model.sliderValue
+                ]
+                []
+            ]
+        , div []
+            [ div [] [ text ((((model.elapsedTime |> Basics.toFloat) / 10) |> String.fromFloat) ++ "s") ]
+            ]
+        , div []
+            [ div [ style "display" "inline" ] [ text "Duration:" ]
+            , div [ style "display" "inline" ]
+                [ input
+                    [ type_ "range"
+                    , Html.Attributes.min "0"
+                    , value model.sliderValue
+                    , Html.Attributes.max "300"
+                    , onInput ChangeSlider
+                    ]
+                    []
+                ]
+            ]
+        , div []
+            [ button [ style "width" "260px", onClick ResetTimer ] [ text "Reset Timer" ]
+            ]
         ]
